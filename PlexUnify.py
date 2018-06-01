43bc95bf04e824bf1d77c2f56aad7989bd3ac3fa
KBlixt <Sally3320>
2018-06-01 14:16:28 +0200

    working?

--- PlexUnify.py
+++ PlexUnify.py
@@ -32,7 +32,7 @@ except ImportError:
 # global static variables:
 # config stuff
 config_file = 'config.cfg'
-with codecs.open(config_file, 'r', 'utf-8') as open_config_file:
+with codecs.open(os.path.join(os.getcwd(), config_file), 'r', 'utf-8') as open_config_file:
     config = configparser.ConfigParser()
     config.read_file(open_config_file)
 global_settings = config['GLOBAL_SETTINGS']
